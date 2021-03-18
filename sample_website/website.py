55fda22e69f1faf830a772f1d2b3d92d80fb1d6d
“[antea <antea.giljanovic@code.berlin>
2021-03-18 12:06:10 +0100

    Changes

--- sample_website/website.py
+++ sample_website/website.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-<<<<<<< HEAD
     return "<h1>Hello, Chloe!</h1>"
-=======
-    return '<h1>Hello, Class!</h1>'
->>>>>>> upstream/main
-
-
+    
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
